3c2896cf9d429c682aae140382ba2c6e9e77202b
mekutr <mekutr@gmail.com>
2017-10-09 02:13:21 -0400

    ported dashboard v1 to work with rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,23 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery-ui
+//= require dist/resolve_tooltip_conflict
+//= require twitter/bootstrap
+//
+//Dashboard - version1
+//= require morris
+//= require jquery.sparkline
+//= require externals/jvectormap/jquery-jvectormap-1.2.2.min
+//= require externals/jvectormap/jquery-jvectormap-world-mill-en
+//= require jquery.knob
+//= require moment
+//= require externals/daterangepicker
+//= require bootstrap-datepicker/core
+//= require bootstrap-wysihtml5
+//= require externals/jquery.slimscroll
+//= require fastclick
+//=
+//= require dist/adminlte
+//= require dist/pages/dashboard
+//= require dist/demo
---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,11 @@
+class DashboardController < ApplicationController
+
+  def version1
+    @page_title = 'Dashboard_v1'
+  end
+
+  def version2
+
+  end
+
+end
