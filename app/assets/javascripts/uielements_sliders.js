978d309dd17a5c82ed11edb86494bb3cb53339fc
mekutr <mekutr@gmail.com>
2018-02-03 21:25:51 -0500

    implemented the uielements/sliders page

---
+++ app/assets/javascripts/uielements_sliders.js
@@ -0,0 +1,12 @@
+//UIElements-sliders
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require fastclick
+//= require externals/bootstrap-slider
+
+// below includes js calls specifically for the UIElements-sliders page
+$(function () {
+    /* BOOTSTRAP SLIDER */
+    $('.slider').slider()
+});
